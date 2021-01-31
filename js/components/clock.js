766e73677b0fead6144de8ca949811e6bf12c022
Julija <varnaite.j@gmail.com>
2021-01-31 21:14:20 +0200

    Update clock #36

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,7 +1,27 @@
-function clock(selector) {
+function clock(selector, deadline) {
+    // input validation
+    if (typeof selector !== 'string' ||
+        selector === '') {
+            console.error('ERROR: netinkamo formato selektorius.');
+        return false;
+    } 
+
+    if (typeof deadline !== 'string' ||
+        deadline === '' ||
+        !isFinite((new Date(`2000-${deadline}`)).getTime())) {
+        console.error('ERROR: netinkamo formato deadline reiksme.');
+        return false;
+    } 
+
+
+    // logic
     const clockDOM = document.querySelector(selector);
+    if (!clockDOM) {
+        console.error('ERROR: pagal pateikta selektoriu nepavyko rasti norimo DOM elemento');
+        return false;
+    }
+
     let allValuesDOM = null;
-    const deadline = '01-04 14:00:00';
     let numbers = calcTime(deadline);
     const labels = ['days', 'hours', 'minutes', 'seconds'];
     let HTML = '';
