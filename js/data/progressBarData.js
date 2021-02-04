89055e8b4ab6d4decb9d41720382d03497b8e473
Julijava <76951723+Julijava@users.noreply.github.com>
2021-02-04 14:55:01 +0200

    Progress bars begins #19 #22

---
+++ js/data/progressBarData.js
@@ -0,0 +1,17 @@
+const progressBarData = [
+    {
+        label: 'UX Design',
+        value: 90
+    },
+    {
+        label: 'Web Design',
+        value: 86
+    },
+    {
+        label: 'Web development',
+        value: 50
+    },
+];
+
+
+export { progressBarData }
